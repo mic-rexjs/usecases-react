eab0d5a7336197129bbb5986ec4e8fbbef93248f
liji <hello_zaizi@163.com>
2023-10-11 16:03:54 +0800

    feat: 入口文件新增输出部分常用类型定义

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,6 @@
-export * from './components/UseCaseFragment';
-export * from './hooks/useUseCase';
+export { UseCaseFragment } from './components/UseCaseFragment';
+export { useUseCase } from './hooks/useUseCase';
+
+export type { UseCaseFragmentProps } from './components/UseCaseFragment/types';
+export type { UseCaseContext, UseCaseContextWithProvider, UseCaseProvider } from './configs/usecaseContextMap/types';
+export type { UseCaseHookOptions } from './hooks/useUseCase/types';
