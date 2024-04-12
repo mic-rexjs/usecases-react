369e03520b98b89274a2c8b1bac4f04f3731a534
liji <hello_zaizi@163.com>
2024-04-13 00:40:47 +0800

    perf: 根文件新增类型输出 EntityWatchEvent

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,13 @@ export { UseCaseFragment } from './components/UseCaseFragment';
 export { useUseCase } from './hooks/useUseCase';
 export { useStatelessUseCase } from './hooks/useStatelessUseCase';
 
-export type { RootCoreCollection, ContextualCoreCollection, CoreCollection } from './hooks/useUseCase/types';
+export type {
+  EntityWatchEvent,
+  RootCoreCollection,
+  ContextualCoreCollection,
+  CoreCollection,
+} from './hooks/useUseCase/types';
+
 export type { UseCaseHookOptions } from './hooks/useUseCase/types';
 export type { UseCaseProvider } from './hooks/useProvider/types';
 export type { UseCaseFragmentProps } from './components/UseCaseFragment/types';
